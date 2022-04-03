4ad6b6683a1932f28c16588a8b81cbe2cff8dcac
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-03 23:12:39 +0300

    Update play.py

--- Music/Plugins/play.py
+++ Music/Plugins/play.py
@@ -330,7 +330,7 @@ Saya perlu menjadi admin dengan beberapa izin:
             else file_name,
         )
         title = "Audio Yang Dipilih Dari Telegram"
-        link = "https://t.me/NastyProject"
+        link = "https://t.me/xl444"
         thumb = "cache/Audio.png"
         videoid = "smex1"
     elif url:
