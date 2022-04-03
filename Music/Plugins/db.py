d0a2a386fa6b6400b0d93d8d0b71dc77ddac485a
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-03 04:46:52 +0300

    Update db.py

--- Music/Plugins/db.py
+++ Music/Plugins/db.py
@@ -6,27 +6,27 @@ from Music.config import OWNER_ID
 from Music.MusicUtilities.tgcallsrun import ASS_ACC as USER
 
 
-@Client.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & ~filters.edited)
+@Client.on_message(filters.command("اذاعة عام") & filters.user(OWNER_ID) & ~filters.edited)
 async def gcast(_, message: Message):
     sent=0
     failed=0
     if message.from_user.id not in OWNER_ID:
         return
     else:
-        wtf = await message.reply("Sedang mengirim pesan global...")
+        wtf = await message.reply("اذاعة طارئة ، تم...")
         if not message.reply_to_message:
-            await wtf.edit("Balas pesan teks apa pun untuk broadcast")
+            await wtf.edit("رد على رسالة مو هيج")
             return
         lmao = message.reply_to_message.text
         async for dialog in USER.iter_dialogs():
             try:
                 await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"Sedang mengirim pesan global \n\nTerkirim ke: {sent} chat \nGagal terkirim ke: {failed} chat")
+                await wtf.edit(f"يحري الاذاعة \n\nمرسلة إلى: {sent} دردشة \nفشل الإرسال إلى: {failed} دردشة")
                 await asyncio.sleep(0.7)
             except:
                 failed=failed+1
-                await wtf.edit(f"Sedang mengirim pesan global \n\nTerkirim ke: {sent} Chats \nGagal terkirim ke: {failed} Chats")
+                await wtf.edit(f"يجري الاذاعة \n\nمرسلة إلى: {sent} دردشة \nفشل الإرسال إلى: {failed} دردشة")
                 await asyncio.sleep(0.7)
 
-        await message.reply_text(f"Pesan global selesai \n\nTerkirim ke: {sent} Chats \nGagal terkirim ke: {failed} Chats")
+        await message.reply_text(f"تم الاذاعة عزيزي المطور 🧑‍💻 \n\nمرسلة إلى: {sent} دردشة \nفشل الإرسال إلى: {failed} دردشة")
