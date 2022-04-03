ee8c33a4f51ab48ab0a7ad1124dbcf394e3c5ae0
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-03 14:37:12 +0300

    Update speedtest.py

--- Music/Plugins/speedtest.py
+++ Music/Plugins/speedtest.py
@@ -7,7 +7,7 @@ from pyrogram import filters, Client
 from Music.MusicUtilities.database.onoff import (is_on_off, add_on, add_off)
 from pyrogram.types import Message
 
-@app.on_message(filters.command("speedtest") & ~filters.edited)
+@app.on_message(filters.command("السرعة") & ~filters.edited)
 async def gstats(_, message):
     userid = message.from_user.id
     if await is_on_off(2):
@@ -15,34 +15,34 @@ async def gstats(_, message):
             pass
         else:
             return
-    m = await message.reply_text("__Running KyySpeedTest From Server__")
+    m = await message.reply_text("__بدء رفع ملفات السرعة 🧑‍💻__")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = await m.edit("📥 Mengunduh Hasil SpeedTest")
+        m = await m.edit("🧑‍💻 تحميل نتائج السرعة")
         test.download()
-        m = await m.edit("📤 Mengunggah Hasil SpeedTest")
+        m = await m.edit("🧑‍💻 تحميل ملفات السرعة")
         test.upload()
         test.results.share()
         result = test.results.dict()
     except Exception as e:
         await message.err(text=e)
         return 
-    m = await m.edit("📲 Hasil SpeedTest")
+    m = await m.edit("📲 ترتيب النتائج")
     path = wget.download(result["share"])
-    output = f"""**📜 Berbagi Hasil SpeedTest**
+    output = f"""**📜 النتائج**
     
-<u> **Client:**</u>
+<u> **العميل:**</u>
 
-**__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
+**__مزود خدمة الإنترنت:__** {result['client']['isp']}
+**__دولة:__** {result['client']['country']}
   
-<u> **Server:**</u>
+<u> **الخادم:**</u>
 
-**__Nama:__** {result['server']['name']}
-**__Negara:__** {result['server']['country']}, {result['server']['cc']}
-**__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
+**__الاسم:__** {result['server']['name']}
+**__دولة:__** {result['server']['country']}, {result['server']['cc']}
+**__كفيل:__** {result['server']['sponsor']}
+**__وقت الإستجابة:__** {result['server']['latency']}  
 
 **__Ping:__** {result['ping']}"""
     msg = await app.send_photo(
