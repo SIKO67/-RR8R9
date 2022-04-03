1ac58e59f27796cf3489e302f2f24717406a1731
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-03 19:30:41 +0300

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -60,8 +60,8 @@ pstart_markup = InlineKeyboardMarkup(
         ],
         [
             InlineKeyboardButton(text="• اެݪاۅاެمࢪ​", url=f"https://telegra.ph/%D9%85%D8%B1%D8%AD%D8%A8%D8%A7--%D8%AA%D8%A7%D8%A8%D8%B9-%D8%A7%D9%84%D8%A3%D8%B3%D9%81%D9%84-04-03"),
-            InlineKeyboardButton("• MᥲᎥꪀƚᥲᎥꪀᥱძ ხy", url=f"https://t.me/RR3R2"),
-            InlineKeyboardButton("• MᥲᎥꪀƚᥲᎥꪀᥱძ ხy", url=f"https://t.me/RR3R2"),
+            InlineKeyboardButton("• ხy ", url=f"https://t.me/RR3R2"),
+            InlineKeyboardButton("• قناެة اެݪمطۅࢪ ", url=f"https://t.me/{CHANNEL}"),
         ], 
     ]
 )
