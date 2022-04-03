04d78ae78707a68198bd0e072756d36182c6cb7a
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-03 14:23:55 +0300

    Update sudo.py

--- Music/Plugins/sudo.py
+++ Music/Plugins/sudo.py
@@ -6,12 +6,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 
-@app.on_message(filters.command("addsudo") & filters.user(OWNER))
+@app.on_message(filters.command("رفع مطور") & filters.user(OWNER))
 async def useradd(_, message: Message):
     if not message.reply_to_message:
         if len(message.command) != 2:
             await message.reply_text(
-                "Balas pesan pengguna atau berikan nama pengguna/id_pengguna."
+                "الرد على رسائل المستخدم أو تقديم اسم المستخدم."
             )
             return
         user = message.text.split(None, 1)[1]
@@ -25,7 +25,7 @@ async def useradd(_, message: Message):
         added = await add_sudo(user.id)
         if added:
             await message.reply_text(
-                f"Ditambahkan **{user.mention}** Sebagai Pengguna sudo"
+                f"تم رفع **{user.mention}** كـ مطور في البوت بنجاح"
             )
             return os.execvp("python3", ["python3", "-m", "Music"])
         await edit_or_reply(message, text="Terjadi kesalahan, periksa log.")
@@ -35,21 +35,21 @@ async def useradd(_, message: Message):
     mention = message.reply_to_message.from_user.mention
     sudoers = await get_sudoers()
     if user_id in sudoers:
-        return await message.reply_text("Sudah menjadi Pengguna Sudo.")
+        return await message.reply_text("بالفعل تم رفعة في قائمة المطورين.")
     added = await add_sudo(user_id)
     if added:
-        await message.reply_text(f"Ditambahkan **{mention}** Sebagai Pengguna Sudo")
+        await message.reply_text(f"تم اضافة **{mention}** الى قائمة المطورين")
         return os.execvp("python3", ["python3", "-m", "Music"])
     await edit_or_reply(message, text="Terjadi kesalahan, periksa log.")
     return
 
 
-@app.on_message(filters.command("delsudo") & filters.user(OWNER))
+@app.on_message(filters.command("تنزيل مطور") & filters.user(OWNER))
 async def userdel(_, message: Message):
     if not message.reply_to_message:
         if len(message.command) != 2:
             await message.reply_text(
-                "Membalas pesan pengguna atau memberikan nama pengguna/id_pengguna."
+                "الرد على رسالة مستخدم أو تقديم اسم مستخدم."
             )
             return
         user = message.text.split(None, 1)[1]
@@ -58,29 +58,29 @@ async def userdel(_, message: Message):
         user = await app.get_users(user)
         message.from_user
         if user.id not in await get_sudoers():
-            return await message.reply_text(f"Not a part of Music's Sudo.")
+            return await message.reply_text(f"بالفعل تم تنزيلة.")
         removed = await remove_sudo(user.id)
         if removed:
-            await message.reply_text(f"Menghapus **{user.mention}** dari Sudo.")
+            await message.reply_text(f"تم حذف **{user.mention}** من المطورين.")
             return os.execvp("python3", ["python3", "-m", "Music"])
-        await message.reply_text(f"Sesuatu yang salah terjadi.")
+        await message.reply_text(f"حدث شيء خطأ.")
         return
     message.from_user.id
     user_id = message.reply_to_message.from_user.id
     mention = message.reply_to_message.from_user.mention
     if user_id not in await get_sudoers():
-        return await message.reply_text(f"Bukan bagian dari Sudo Musik.")
+        return await message.reply_text(f"بالفعل تم حذفة.")
     removed = await remove_sudo(user_id)
     if removed:
-        await message.reply_text(f"Menghapus **{mention}** dari Sudo.")
+        await message.reply_text(f"حذف **{mention}** من المطورين.")
         return os.execvp("python3", ["python3", "-m", "Music"])
     await message.reply_text(f"Something wrong happened.")
 
 
-@app.on_message(filters.command("sudolist"))
+@app.on_message(filters.command("المطورين"))
 async def sudoers_list(_, message: Message):
     sudoers = await get_sudoers()
-    text = "**Daftar Pengguna Sudo**\n\n"
+    text = "**قائمة مطورين البوت 🧑‍💻 تم برمجة كل هذه العمل بواسطة @rr8r9**\n\n"
     for count, user_id in enumerate(sudoers, 1):
         try:
             user = await app.get_users(user_id)
@@ -89,6 +89,6 @@ async def sudoers_list(_, message: Message):
             continue
         text += f"• {user}\n"
     if not text:
-        await message.reply_text("Tidak Ada Pengguna Sudo")
+        await message.reply_text("لايوجد مطورين ")
     else:
         await message.reply_text(text)
