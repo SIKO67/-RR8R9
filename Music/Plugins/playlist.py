7cd403b80c38071e28d1f223bd68a7e65dbbdd2b
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-03 14:53:46 +0300

    Update playlist.py

--- Music/Plugins/playlist.py
+++ Music/Plugins/playlist.py
@@ -103,19 +103,19 @@ from pyrogram.types import (
 options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "all","16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",]   
 
 
-@app.on_message(filters.command("playlist"))
+@app.on_message(filters.command("الانتضار"))
 async def pause_cmd(_, message):
     thumb ="cache/IMG_20211230_211509_034.jpg"
     await message.reply_photo(
     photo=thumb, 
-    caption=("**__Music's Playlist Feature__**\n\nSelect The Playlist, You want to check!"),    
+    caption=("**__ميزة قائمة تشغيل الاغاني__**\n\nحدد قائمة التشغيل, تريد التحقق!"),    
     reply_markup=play_list_keyboard) 
     return 
 
 
-@app.on_message(filters.command("delmyplaylist"))
+@app.on_message(filters.command("حذف الانتضار"))
 async def pause_cmd(_, message):
-    usage = ("Usage:\n\n/delmyplaylist [Numbers between 1-30] ( to delete a particular music in playlist )\n\nor\n\n /delmyplaylist all ( to delete whole playlist )")
+    usage = ("الاستخدام:\n\n/حذف الانتضار [أرقام بين 1-30] ( لحذف موسيقى معينة في قائمة التشغيل )\n\nاو\n\n /حذف الانتضار الكل ( لحذف موسيقى معينة في قائمة التشغيل )")
     if len(message.command) < 2:
         return await message.reply_text(usage)
     name = message.text.split(None, 1)[1].strip()
@@ -124,11 +124,11 @@ async def pause_cmd(_, message):
     if name not in options:
         return await message.reply_text(usage)
     if len(message.text) == 18:
-        return await message.reply_text(f"Confirmation!!\nYou sure you want to delete your whole playlist?", reply_markup=confirm_keyboard)
+        return await message.reply_text(f"تأكيد!!\nأنت متأكد أنك تريد حذف قائمة التشغيل بأكملها?", reply_markup=confirm_keyboard)
     else:
          _playlist = await get_note_names(message.from_user.id)
     if not _playlist:
-        await message.reply_text("You have no Playlist on Music's Server")
+        await message.reply_text("ليس لديك قائمة تشغيل على خادم الاغاني")
     else:
         titlex = []
         j = 0
@@ -139,18 +139,18 @@ async def pause_cmd(_, message):
             if j == count:
                 deleted = await delete_playlist(message.from_user.id, note)
                 if deleted:
-                    return await message.reply_text(f"**Deleted the {count} music in playlist**")
+                    return await message.reply_text(f"**تم حذف ملف {count} تم حذف ملف**")
                 else:
-                    return await message.reply_text(f"**No such saved music in playlist.**")                                
-        await message.reply_text("You have no such music in Playlist.")                             
+                    return await message.reply_text(f"**لا توجد مثل هذه الموسيقى المحفوظة في قائمة التشغيل.**")                                
+        await message.reply_text("ليس لديك مثل هذه الموسيقى في قائمة التشغيل.")                             
 
         
 @app.on_message(filters.command("delgroupplaylist"))
 async def delgroupplaylist(_, message):
     a = await app.get_chat_member(message.chat.id , message.from_user.id)
     if not a.can_manage_voice_chats:
-        return await message.reply_text("I don't have the required permission to perform this action.\n**Permission:** __MANAGE VOICE CHATS__")
-    usage = ("Usage:\n\n/delgroupplaylist [Numbers between 1-30] ( to delete a particular music in playlist )\n\nor\n\n /delgroupplaylist all ( to delete whole playlist )")
+        return await message.reply_text("ليس لدي الإذن المطلوب لتنفيذ هذا الإجراء.\n**الاذن:** __دردشة الفيديو__")
+    usage = ("الاستخدام:\n\n//ماسح [أرقام بين 1-30] ( لحذف موسيقى معينة في قائمة التشغيل )\n\nor\n\n /ماسح  ( لحذف قائمة التشغيل بأكملها )")
     if len(message.command) < 2:
         return await message.reply_text(usage)
     name = message.text.split(None, 1)[1].strip()
@@ -159,11 +159,11 @@ async def delgroupplaylist(_, message):
     if name not in options:
         return await message.reply_text(usage)
     if len(message.text) == 21:
-        return await message.reply_text(f"Confirmation!!\nYou sure you want to delete whole whole playlist?", reply_markup=confirm_group_keyboard)
+        return await message.reply_text(f"تأكيد!!\nأنت متأكد أنك تريد حذف قائمة التشغيل بأكملها?", reply_markup=confirm_group_keyboard)
     else:
          _playlist = await get_note_names(message.chat.id)
     if not _playlist:
-        await message.reply_text("Group has no Playlist on Music's Server")
+        await message.reply_text("لا تحتوي المجموعة على قائمة تشغيل على خادم الاغاني")
     else:
         titlex = []
         j = 0
@@ -174,7 +174,7 @@ async def delgroupplaylist(_, message):
             if j == count:
                 deleted = await delete_playlist(message.chat.id, note)
                 if deleted:
-                    return await message.reply_text(f"**Deleted the {count} music in group's playlist**")
+                    return await message.reply_text(f"**تم حذف {count} من الاغاني**")
                 else:
-                    return await message.reply_text(f"**No such saved music in Group playlist.**")                                
-        await message.reply_text("You have no such music in Group Playlist.")
+                    return await message.reply_text(f"**لا توجد مثل هذه الموسيقى المحفوظة في قائمة التشغيل الجماعية.**")                                
+        await message.reply_text("ليس لديك مثل هذه الموسيقى في قائمة التشغيل الجماعية.")
