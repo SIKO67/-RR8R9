d1dd93c0ffddf7ef94938c57257137fba3e3a3f9
freedomSource <102356034+freedomSource@users.noreply.github.com>
2022-04-04 13:44:22 +0300

    Update logger.py

--- Music/MusicUtilities/helpers/logger.py
+++ Music/MusicUtilities/helpers/logger.py
@@ -6,18 +6,18 @@ async def LOG_CHAT(message, what):
     if message.chat.username:
         chatusername = (f"@{message.chat.username}")
     else:
-        chatusername = ("Private Group")
+        chatusername = ("مجموعة خاصة")
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     mention = "["+user_name+"](tg://user?id="+str(user_id)+")" 
     logger_text = f"""
-__**New {what}**__
+__** 🧑‍💻 عزيزي المطور قام احد بتشغيل البوت ،اليك المعلومات {what}**__
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
-**Query:** {message.text}"""
+**🧑‍💻الدردشة:** {message.chat.title} [`{message.chat.id}`]
+**🧑‍💻المستخدم:** {mention}
+**🧑‍💻المعرف:** @{message.from_user.username}
+**🧑‍💻ايدي المستخدم:** `{message.from_user.id}`
+**🧑‍💻رابط المجموعة:** {chatusername}
+**🧑‍💻الكلمة التي بحثها:** {message.text}"""
     await app.send_message(LOG_GROUP_ID, f"{logger_text}", disable_web_page_preview=True)
     
